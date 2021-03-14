ccbc110d1ffe6cfe65eb32c10e04efc60986dcd1
Abigail <abigailthelin@workfront.com>
2021-03-13 18:06:35 -0700

    fix: updated port

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var app = (module.exports = express());
 
 app.use(express.static(__dirname + "/public"));
 
-var port = 3000
+var PORT = process.env.PORT || 3000
 
 app.get("*", function(req, res) {
     res.send(path.join(__dirname, "./public/build/bundle.js"));
 });
 
-app.listen(port, function() {
+app.listen(PORT, function() {
     console.log("App is up and running");
 });
